2f7216db033b5d3b011fde589cbf9dbb1fb71217
Tiago Pomella Lobo <tiago_p_lobo@hotmail.com>
2024-08-05 19:16:32 +0200

    add indentation and small corrections

--- restore_spaces/rs/environment.lua
+++ restore_spaces/rs/environment.lua
@@ -108,154 +108,154 @@ return function(rs,hs)
         return answer
     end
 
-rs.processEnvironment = function(save_flag)
-    local function sortByFrame(a, b)
-        return a:frame().x < b:frame().x
-    end
-    local function listKeys(table)
-        local keys_list = ""
-        for key, _ in pairs(table) do
-            keys_list = keys_list .. "'" .. key .. "'\n"
+    rs.processEnvironment = function(save_flag)
+        local function sortByFrame(a, b)
+            return a:frame().x < b:frame().x
         end
-        return keys_list
-    end
-
-    rs.data_envs = rs.processDataInFile("read","environments")
-    
-    local all_screens = rs.retrieveEnvironmentEntities("screens")
-    table.sort(all_screens, sortByFrame)
-    
-    local env = rs.detectEnvironment(all_screens)
-    local env_exists, env_name = rs.validateEnvironment(env)
-
-    if env_exists then
-        rs.rebuildEnvironment(env, env_name, all_screens, save_flag)
-        --[[
-        -- FOR TESTING ONLY:
-        local envs_list = listKeys(mod.data_envs)
-        env_name = mod.askEnvironmentName(envs_list, mod.verbose)
-        if not env_name then
-            error("Undefined environment name: !")
-        else
-            mod.data_envs[env_name] = env
+        local function listKeys(table)
+            local keys_list = ""
+            for key, _ in pairs(table) do
+                keys_list = keys_list .. "'" .. key .. "'\n"
+            end
+            return keys_list
         end
-        --]]
-    else
-        local text = "Environment does not exist."
-        rs.issueVerbose(text, rs.verbose)
-        text = "Environment name undefined!"
-        if save_flag then
-            local envs_list = listKeys(rs.data_envs)
-            env_name = rs.askEnvironmentName(envs_list, rs.verbose)
+
+        rs.data_envs = rs.processDataInFile("read","environments")
+        
+        local all_screens = rs.retrieveEnvironmentEntities("screens")
+        table.sort(all_screens, sortByFrame)
+        
+        local env = rs.detectEnvironment(all_screens)
+        local env_exists, env_name = rs.validateEnvironment(env)
+
+        if env_exists then
+            rs.rebuildEnvironment(env, env_name, all_screens, save_flag)
+            --[[
+            -- FOR TESTING ONLY:
+            local envs_list = listKeys(mod.data_envs)
+            env_name = mod.askEnvironmentName(envs_list, mod.verbose)
             if not env_name then
-                error(text)
+                error("Undefined environment name: !")
             else
-                rs.data_envs[env_name] = env
+                mod.data_envs[env_name] = env
             end
-            rs.data_envs = rs.processDataInFile("write","environments")
+            --]]
         else
-            rs.notifyUser("environment")
-            error(text)
+            local text = "Environment does not exist."
+            rs.issueVerbose(text, rs.verbose)
+            text = "Environment name undefined!"
+            if save_flag then
+                local envs_list = listKeys(rs.data_envs)
+                env_name = rs.askEnvironmentName(envs_list, rs.verbose)
+                if not env_name then
+                    error(text)
+                else
+                    rs.data_envs[env_name] = env
+                end
+                rs.data_envs = rs.processDataInFile("write","environments")
+            else
+                rs.notifyUser("environment")
+                error(text)
+            end
         end
+
+        return env_name, env
     end
 
-    return env_name, env
-end
-
-rs.detectEnvironment = function(all_screens)
-    local env = {}
-    for screen_i, screen in ipairs(all_screens) do
-        local screen_name = screen:name()
-        local screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
-        local screen_index = rs.paddedToStr(screen_i)
-        local space_map = {}
-        for space_i, space in ipairs(screen_spaces) do
-            local space_index = rs.paddedToStr(space_i)
-            --TODO: add docstrings that explain that the first value is
-            --      the original space id during `save`, and the second
-            --      is the current space id during `apply`
-            space_map[space_index] = {space, space}
-        end
-        env[screen_index] = {
-            ["monitor"] = screen_name,
-            ["space_map"] = space_map
-        }
-    end
-    return env
-end
-
-rs.validateEnvironment = function(env)
-    local env_exists = false
-    local env_name
-    for saved_name, saved_env in pairs(rs.data_envs) do
-        local saved_monitors = {}
-        for _, value in pairs(saved_env) do
-            table.insert(saved_monitors, value["monitor"])
-        end
-
-        local current_monitors = {}
-        for _, value in pairs(env) do
-            table.insert(current_monitors, value["monitor"])
-        end
-
-        local check_monitors = (
-            hs.inspect(saved_monitors) == hs.inspect(current_monitors)
-        )
-        if check_monitors then
-            env_exists = true
-            env_name = saved_name
-            break
-        end
-    end
-    return env_exists, env_name
-end
-
-rs.rebuildEnvironment = function(env, env_name, all_screens, save_flag)
-    local function lengthTables(...)
-        local args = {...}
-        local all_counts = {}
-        for i, arg in ipairs(args) do
-            local count = 0
-            for _ in pairs(arg) do count = count + 1 end
-            all_counts[i] = count
-        end
-        return table.unpack(all_counts)
-    end
-
-    if save_flag then
-        rs.issueVerbose("Overwriting space order and map...", rs.verbose)
-    else
-        rs.issueVerbose("Re-building environment...", rs.verbose)
-        local saved_env = rs.data_envs[env_name]
+    rs.detectEnvironment = function(all_screens)
+        local env = {}
         for screen_i, screen in ipairs(all_screens) do
-            local screen_index = rs.paddedToStr(screen_i)
+            local screen_name = screen:name()
             local screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
-            local saved_map = saved_env[screen_index]["space_map"]
-            local n_screen, n_saved = lengthTables(screen_spaces, saved_map)
-            local close_MissionControl = false
-            while n_screen < n_saved do
-                hs.spaces.addSpaceToScreen(screen_i, close_MissionControl)
-                screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
-                n_screen, n_saved = lengthTables(screen_spaces, saved_map)
-            end
-            while n_screen > n_saved do
-                local last_space_id = screen_spaces[n_screen]
-                hs.spaces.removeSpace(last_space_id, close_MissionControl)
-                screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
-                n_screen, n_saved = lengthTables(screen_spaces, saved_map)
-            end
-            local screen_map = {}
+            local screen_index = rs.paddedToStr(screen_i)
+            local space_map = {}
             for space_i, space in ipairs(screen_spaces) do
                 local space_index = rs.paddedToStr(space_i)
-                local original_space = saved_map[space_index][1]
-                screen_map[space_index] = {original_space, space}
+                --TODO: add docstrings that explain that the first value is
+                --      the original space id during `save`, and the second
+                --      is the current space id during `apply`
+                space_map[space_index] = {space, space}
             end
-            env[screen_index]["space_map"] = screen_map
-            rs.issueVerbose("env: " .. hs.inspect(env), rs.verbose)
+            env[screen_index] = {
+                ["monitor"] = screen_name,
+                ["space_map"] = space_map
+            }
         end
+        return env
     end
-    rs.data_envs[env_name] = env
-    rs.processDataInFile("write","environments")
-end
+
+    rs.validateEnvironment = function(env)
+        local env_exists = false
+        local env_name
+        for saved_name, saved_env in pairs(rs.data_envs) do
+            local saved_monitors = {}
+            for _, value in pairs(saved_env) do
+                table.insert(saved_monitors, value["monitor"])
+            end
+
+            local current_monitors = {}
+            for _, value in pairs(env) do
+                table.insert(current_monitors, value["monitor"])
+            end
+
+            local check_monitors = (
+                hs.inspect(saved_monitors) == hs.inspect(current_monitors)
+            )
+            if check_monitors then
+                env_exists = true
+                env_name = saved_name
+                break
+            end
+        end
+        return env_exists, env_name
+    end
+
+    rs.rebuildEnvironment = function(env, env_name, all_screens, save_flag)
+        local function lengthTables(...)
+            local args = {...}
+            local all_counts = {}
+            for i, arg in ipairs(args) do
+                local count = 0
+                for _ in pairs(arg) do count = count + 1 end
+                all_counts[i] = count
+            end
+            return table.unpack(all_counts)
+        end
+
+        if save_flag then
+            rs.issueVerbose("Overwriting space order and map...", rs.verbose)
+        else
+            rs.issueVerbose("Re-building environment...", rs.verbose)
+            local saved_env = rs.data_envs[env_name]
+            for screen_i, screen in ipairs(all_screens) do
+                local screen_index = rs.paddedToStr(screen_i)
+                local screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
+                local saved_map = saved_env[screen_index]["space_map"]
+                local n_screen, n_saved = lengthTables(screen_spaces, saved_map)
+                local close_MissionControl = false
+                while n_screen < n_saved do
+                    hs.spaces.addSpaceToScreen(screen_i, close_MissionControl)
+                    screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
+                    n_screen, n_saved = lengthTables(screen_spaces, saved_map)
+                end
+                while n_screen > n_saved do
+                    local last_space_id = screen_spaces[n_screen]
+                    hs.spaces.removeSpace(last_space_id, close_MissionControl)
+                    screen_spaces = rs.retrieveEnvironmentEntities("spaces", screen)
+                    n_screen, n_saved = lengthTables(screen_spaces, saved_map)
+                end
+                local screen_map = {}
+                for space_i, space in ipairs(screen_spaces) do
+                    local space_index = rs.paddedToStr(space_i)
+                    local original_space = saved_map[space_index][1]
+                    screen_map[space_index] = {original_space, space}
+                end
+                env[screen_index]["space_map"] = screen_map
+                rs.issueVerbose("env: " .. hs.inspect(env), rs.verbose)
+            end
+        end
+        rs.data_envs[env_name] = env
+        rs.processDataInFile("write","environments")
+    end
 
 end
